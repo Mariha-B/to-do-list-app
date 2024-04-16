2853f1c37d2810468f2daeb9352629721fae345a
Mariha-B <111390252+Mariha-B@users.noreply.github.com>
2024-04-16 16:51:11 +0100

    Initial commit

---
+++ ToDoListApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ToDoListApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void HandleTextChanged(object sender, TextChangedEventArgs e)
+        {
+            Console.WriteLine(e.NewTextValue);
+        }
+
+        public void HandleEnterPressed(object sender, EventArgs e)
+        {
+            Console.WriteLine("Enter Pressed");
+            
+        }
+    }
+}
